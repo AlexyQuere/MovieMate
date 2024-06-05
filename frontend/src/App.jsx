7430f468004eef2cbe9127a732f5634daf6d0d74
Davadant Thomas <thomas.davadant@student-cs.fr>
2024-06-05 10:36:19 +0200

    fix/appjsxwithpagesandcontainers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import Movie_genre from './components/Movie_genre/Movie_genre';
-import Home from './pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Movie_by_genre from './pages/Movie_by_genre/Movie_by_genre';
+import About from './pages/About/About';
+import Layout from './components/Layout/Layout';
+import Counter from './pages/Counter/Counter';
+import Users from './pages/Users/Users';
 
-const App = () => {
-  const [checkedValue, setCheckedValue] = useState('');
-
+function App() {
   return (
-    <div>
-      <Movie_genre setCheckedValue={setCheckedValue} />
-      <Home checkedValue={checkedValue} />
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Movie_by_genre />} />
+        <Route path="counter" element={<Counter />} />
+        <Route path="users" element={<Users />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Layout>
   );
-};
+}
 
 export default App;
