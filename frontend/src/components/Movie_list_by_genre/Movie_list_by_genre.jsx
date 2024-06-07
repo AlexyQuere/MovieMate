85f884a35bc4aa6168c44c4f799f218f8287254b
Davadant Thomas <thomas.davadant@student-cs.fr>
2024-06-07 11:52:36 +0200

    some changes plus search bar

--- frontend/src/components/Movie_list_by_genre/Movie_list_by_genre.jsx
+++ frontend/src/components/Movie_list_by_genre/Movie_list_by_genre.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // Assurez-vous d'importer axios si vous l'utilisez
+import axios from 'axios';
 import Movie from '../Moviecard/Moviecard';
 import './Movie_list_by_genre.css';
 
 const Movie_list_by_genre = ({ checkedValue }) => {
   const [movies, setMovies] = useState([]);
-  const [startIndex, setStartIndex] = useState(0); // Index du premier film à afficher
-  const [searchTerm, setSearchTerm] = useState(''); // État pour le terme de recherche
+  const [startIndex, setStartIndex] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Vérifiez si checkedValue est défini ou vide avant de faire la requête
     const fetchMovies = async () => {
       try {
         const response = await axios.get(
@@ -20,28 +19,24 @@ const Movie_list_by_genre = ({ checkedValue }) => {
         console.log(response);
         setMovies(response.data.movies || []);
       } catch (error) {
-        console.error('Erreur lors de la récupération des films:', error);
+        console.error('Error fetching movies:', error);
       }
     };
 
     fetchMovies();
   }, [checkedValue]);
 
-  // Filtrer les films en fonction du terme de recherche
   const filteredMovies = movies.filter(
     (movie) =>
-      // Vérifiez que le titre est défini avant d'appeler toLowerCase
       movie.name && movie.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // Fonction pour gérer le clic de l'icône pour afficher les films suivants
   const handleNextClick = () => {
-    setStartIndex(startIndex + 6); // Incrémente l'index de 6
+    setStartIndex(startIndex + 6);
   };
 
-  // Fonction pour gérer le clic de l'icône pour afficher les films précédents
   const handlePrevClick = () => {
-    setStartIndex(Math.max(0, startIndex - 6)); // Décrémente l'index de 6, en s'assurant qu'il ne soit pas inférieur à 0
+    setStartIndex(Math.max(0, startIndex - 6));
   };
 
   return (
@@ -68,52 +63,54 @@ const Movie_list_by_genre = ({ checkedValue }) => {
           <Movie key={movie.id} movie={movie} />
         ))}
       </div>
-      {startIndex > 0 && ( // Afficher l'icône pour les films précédents si startIndex est supérieur à 0
-        <svg
-          onClick={handlePrevClick}
-          fill="#000000"
-          height="3rem"
-          width="3rem"
-          version="1.1"
-          id="Layer_1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 330 330"
-          xmlSpace="preserve"
-          className="arrow-svg"
-        >
-          <path
-            id="XMLID_6_"
-            d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M205.606,234.394
-	c5.858,5.857,5.858,15.355,0,21.213C202.678,258.535,198.839,260,195,260s-7.678-1.464-10.606-4.394l-80-79.998
-	c-2.813-2.813-4.394-6.628-4.394-10.606c0-3.978,1.58-7.794,4.394-10.607l80-80.002c5.857-5.858,15.355-5.858,21.213,0
-	c5.858,5.857,5.858,15.355,0,21.213l-69.393,69.396L205.606,234.394z"
-          />
-        </svg>
-      )}
-      {filteredMovies.length > startIndex + 6 && ( // Afficher l'icône pour les films suivants si plus de films sont disponibles
-        <svg
-          onClick={handleNextClick}
-          fill="#000000"
-          height="3rem"
-          width="3rem"
-          version="1.1"
-          id="Layer_1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 330 330"
-          xmlSpace="preserve"
-          className="arrow-svg"
-        >
-          <path
-            id="XMLID_2_"
-            d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M225.606,175.605
-       l-80,80.002C142.678,258.535,138.839,260,135,260s-7.678-1.464-10.606-4.394c-5.858-5.857-5.858-15.355,0-21.213l69.393-69.396
-       l-69.393-69.392c-5.858-5.857-5.858-15.355,0-21.213c5.857-5.858,15.355-5.858,21.213,0l80,79.998
-       c2.814,2.813,4.394,6.628,4.394,10.606C230,168.976,228.42,172.792,225.606,175.605z"
-          />
-        </svg>
-      )}
+      <div className="pagination">
+        {startIndex > 0 && (
+          <svg
+            onClick={handlePrevClick}
+            fill="#000000"
+            height="3rem"
+            width="3rem"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 330 330"
+            xmlSpace="preserve"
+            className="arrow-svg"
+          >
+            <path
+              id="XMLID_6_"
+              d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M205.606,234.394
+              c5.858,5.857,5.858,15.355,0,21.213C202.678,258.535,198.839,260,195,260s-7.678-1.464-10.606-4.394l-80-79.998
+              c-2.813-2.813-4.394-6.628-4.394-10.606c0-3.978,1.58-7.794,4.394-10.607l80-80.002c5.857-5.858,15.355-5.858,21.213,0
+              c5.858,5.857,5.858,15.355,0,21.213l-69.393,69.396L205.606,234.394z"
+            />
+          </svg>
+        )}
+        {filteredMovies.length > startIndex + 6 && (
+          <svg
+            onClick={handleNextClick}
+            fill="#000000"
+            height="3rem"
+            width="3rem"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 330 330"
+            xmlSpace="preserve"
+            className="arrow-svg"
+          >
+            <path
+              id="XMLID_2_"
+              d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M225.606,175.605
+              l-80,80.002C142.678,258.535,138.839,260,135,260s-7.678-1.464-10.606-4.394c-5.858-5.857-5.858-15.355,0-21.213l69.393-69.396
+              l-69.393-69.392c-5.858-5.857-5.858-15.355,0-21.213c5.857-5.858,15.355-5.858,21.213,0l80,79.998
+              c2.814,2.813,4.394,6.628,4.394,10.606C230,168.976,228.42,172.792,225.606,175.605z"
+            />
+          </svg>
+        )}
+      </div>
     </div>
   );
 };
--- frontend/src/components/Movieinfo/Movieinfo.jsx
+++ frontend/src/components/Movieinfo/Movieinfo.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import './Movieinfo.css';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Movie = ({ movie }) => {
   const actors = movie.actors.slice(0, 3).map((actor) => actor.name);
-
   const releaseYear = new Date(movie.releasedate).getFullYear();
 
   const actorsString = actors.join(', ');
@@ -17,9 +19,20 @@ const Movie = ({ movie }) => {
           </h1>
           <h2 className="director">by {movie.director.name}</h2>
         </div>
-        <h2>Actors: {actorsString}</h2>
-        <h2>Genres: {genresString}</h2>
+        <h2>Actors : {'  ' + actorsString}</h2>
+        <h2>Genres : {'       ' + genresString}</h2>
         <h3>{movie.synopsis}</h3>
+        <div className="rating-container">
+          <CircularProgressbar
+            value={movie.globalrating * 10}
+            text={`${movie.globalrating * 10}%`}
+            styles={buildStyles({
+              textColor: '#fff',
+              pathColor: '#008000',
+              trailColor: '#333',
+            })}
+          />
+        </div>
       </div>
       <div className="movieimage">
         <img
