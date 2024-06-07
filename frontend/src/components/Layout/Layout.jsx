e6d7d08a62a44ef5f914a7f483c1aebf6f2acad0
Davadant Thomas <thomas.davadant@student-cs.fr>
2024-06-07 14:17:55 +0200

    background fix plus added animations when loading

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,35 +1,9 @@
 import './Layout.css';
-import React, { useEffect, useRef } from 'react';
 import Header from '../Header/Header';
 
 const Layout = ({ children }) => {
-  const appRef = useRef(null);
-
-  useEffect(() => {
-    const moveGradient = (event) => {
-      const winWidth = window.innerWidth;
-      const winHeight = window.innerHeight;
-
-      // Calculate the cursor position as a percentage
-      const mouseX = (event.pageX / winWidth) * 100;
-      const mouseY = (event.pageY / winHeight) * 100;
-
-      if (appRef.current) {
-        appRef.current.style.setProperty('--mouse-x', `${mouseX}%`);
-        appRef.current.style.setProperty('--mouse-y', `${mouseY}%`);
-      }
-    };
-
-    document.addEventListener('mousemove', moveGradient);
-
-    // Clean up the event listener on component unmount
-    return () => {
-      document.removeEventListener('mousemove', moveGradient);
-    };
-  }, []);
-
   return (
-    <div className="Layout-container" ref={appRef}>
+    <div className="Layout-container">
       <Header />
       <div className="Layout-content">{children}</div>
     </div>
